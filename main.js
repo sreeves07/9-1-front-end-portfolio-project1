fe08086096334061593f2f34891d80015ff152ca
sreeves07 <shamarreeves@pursuit.org>
2022-09-13 19:28:09 -0400

    about html & css completed

--- main.js
+++ main.js
@@ -70,9 +70,9 @@ form.addEventListener('submit', (event) => {
             section.innerHTML = `<h2>${name}</h2>\n
             <p><strong>Symbol:</strong> ${symbol}</p>\n
             <p><strong>Rank:</strong> ${rank}</p>\n
-            <p><strong>Current Price:</strong> $ ${dollarPrice}</p>\n
+            <p><strong>Current Price:</strong> $${dollarPrice}</p>\n
             <p><strong>Percent Change (24 hour limit):</strong> ${percentChange}</p>\n
-            <p><strong>Market Cap:</strong> $ ${marketCapPrice}</p>
+            <p><strong>Market Cap:</strong> $${marketCapPrice}</p>
             <p><strong>More Info:</strong> <a href=${link}>More Info</a></p>\n`
             
             id.value = '' 
